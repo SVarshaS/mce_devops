a25c241a22ea1b111c7da51ceb9adecdc0a15db5
Varshitha S <varshithasvarsha7@gamil.com>
2024-07-03 09:08:17 +0530

    again updateddd

--- ex/main.py
+++ ex/main.py
@@ -7,8 +7,8 @@ from division import divide
 def main():
     print("Simple Calculator Program")
     
-    a = 30
-    b = 40
+    a = 40
+    b = 10
     
     print(f"Addition of {a} and {b}: {add(a, b)}")
     print(f"Subtraction of {a} and {b}: {subtract(a, b)}")
